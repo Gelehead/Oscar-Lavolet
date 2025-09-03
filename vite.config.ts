37d697ef40101a8dac4f979186add2383a145ad7
Gelehead <113441042+Gelehead@users.noreply.github.com>
2025-09-03 07:53:19 -0400

    fix github pages again

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Oscar-Lavolet/',
+  base: process.env.NODE_ENV === 'production' ? '/Oscar-Lavolet/' : '/',
   server: {
     port: 3000,
     open: true
